2f61393a689b53a9392f1cf70d0e5e3655befbb5
Ramakun010 <rapstrounut010@gmail.com>
2021-04-07 12:38:40 -0700

    kode pertama

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kalkulator sederhana</title>
+</head>
+<body>
+	<form action="proses.php" method="post">
+		<input type="text" name="angka1">
+		<input type="text" name="angka2">
+		<input type="submit" name="submit" value="Tambahkan"> 
+		
+	</form>
+
+</body>
+</html>
